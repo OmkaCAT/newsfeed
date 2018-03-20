c5800484efee54ab2ada2ecccc4089a542708a92
OmkaCAT <satomakwoodpecker1996@gmail.com>
2018-03-21 01:50:13 +0700

    Fixed

--- newsfeed/settings/__init__.py
+++ newsfeed/settings/__init__.py
@@ -1,13 +1,8 @@
-deploy_env = os.environ.get('DEPLOYMENT_ENVIRONMENT', '')
-if deploy_env == 'production':
+from .base import *
+
+
+SERVER = os.getenv('SERVER')
+if SERVER == 'production':
     from .production import *
-elif deploy_env == 'staging':
-    try:
-        from .production import *
-    except ImportError:
-        pass
 else:
-    try:
-        from .local import *
-    except ImportError:
-        pass
+    from .local import *
