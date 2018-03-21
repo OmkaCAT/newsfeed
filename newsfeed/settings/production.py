765951a443490cc1c5ffef304a9ab3d70c03ab86
OmkaCAT <satomakwoodpecker1996@gmail.com>
2018-03-21 18:21:50 +0700

    Fixed

--- newsfeed/settings/production.py
+++ newsfeed/settings/production.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'zakbc5%^e7n#)k5jw#=m#-=jv+3(#6emxxmr81ywy0-^)=bv1v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['desolate-falls-42433.herokuapp.com']
 
